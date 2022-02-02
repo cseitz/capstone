e3ada0c5844a92bfce1ed5513006c36510c08672
Brandon <btcossin@gmail.com>
2022-02-02 15:48:03 -0500

    changed login page to use material UI

--- src/app/pages/login.tsx
+++ src/app/pages/login.tsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import FormControl from '@mui/material/FormControl'
+import Button from '@mui/material/Button';
+
 export default function login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -8,18 +13,17 @@ export default function login() {
         console.log("username : " + username + "password: " + password)
     }
 
-    return <div style={{ textAlign: 'center', fontSize: 20 }}>
-        <form
-            method="POST"
-            onSubmit={handleSubmit}>
+    return (
+        <Box sx={{ margin: 'auto', width: '25ch', text_align: 'center'}}>
+            <FormControl>
             <span id="errormessage" style={{ color: 'red' }}></span>
             <p>Username</p>
-            <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} placeholder="Username"></input>
+            <TextField type="text" name="username" onChange={(e) => setUsername(e.target.value)} placeholder="Username"></TextField>
             <p>Password</p>
-            <input type="text" name="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input>
+            <TextField type="text" name="password" onChange={(e) => setPassword(e.target.value)} placeholder="Password"></TextField>
             <br></br>
             <br></br>
-            <button type="submit">Log In</button>
-        </form>
-    </div>
+            <Button type="submit" onClick={handleSubmit}>Log In</Button>
+            </FormControl>
+        </Box>);
 }
