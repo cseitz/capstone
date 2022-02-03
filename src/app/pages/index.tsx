297672d7ed3d79e1f69a3f5478dcea186694878f
Nolan Spencer <nspencer3513@gmail.com>
2022-02-03 16:44:12 -0500

    Landing Page WIP

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,9 +1,38 @@
+import { Container, Fab } from "@mui/material";
+import { alignProperty, convertLength } from "@mui/material/styles/cssUtils";
+import { Box } from "@mui/system";
 import { FAQ } from "../ui/components/faq";
 
+const LandingPageOBJ ={
+    eventname: "SAMPLE EVENT",
+    eventdate: "Feb 2nd 2022",
+    backgroundsrc: "https://www.freecodecamp.org/news/content/images/2021/06/w-qjCHPZbeXCQ-unsplash.jpg",
+    logosrc: "https://png.pngtree.com/element_pic/00/16/07/115783931601b5c.jpg",
+};
+
+function LandingPage(){
+    return(
+        <Box sx={{backgroundImage: `url(${LandingPageOBJ.backgroundsrc})`, width: '100%', height: '100%', backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+            <div>
+                <img src={LandingPageOBJ.logosrc}></img>
+                <br/>
+                <h1>{LandingPageOBJ.eventname}</h1>
+                <h2>{LandingPageOBJ.eventdate}</h2>
+                <Fab></Fab>
+            </div>
+        </Box>
+    )
+}
 
 export default function Homepage() {
-    return <div>
-        hi there!
-        <FAQ />
-    </div>
+    return (
+        <div>
+            <Box height='100vh' display="flex" flexDirection="column">
+                <LandingPage/>
+            </Box>
+            <Box height='100vh' display="flex" flexDirection="column">
+                <FAQ/>
+            </Box>
+        </div>
+    )
 }
