50f98a264f07a2c16031d68930890ec3357a212b
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-02-15 20:39:21 -0500

    sass support & basic dark mode

---
+++ src/app/pages/_app.tsx
@@ -0,0 +1,31 @@
+import { createTheme, CssBaseline, useMediaQuery, ThemeProvider } from "@mui/material";
+import { AppProps } from "next/app";
+import { useMemo } from "react";
+import '../ui/styles/global.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+    const prefersDarkMode = useMediaQuery(`(prefers-color-scheme: dark)`);
+    const theme = useMemo(() => {
+        return createTheme({
+            palette: {
+                mode: prefersDarkMode ? 'dark' : 'light',
+                background: {
+                    default: prefersDarkMode ? '#35363a' : 'white',
+                }
+            },
+            components: {
+                MuiPagination: {
+                    styleOverrides: {
+                        ul: {
+                            justifyContent: 'center'
+                        }
+                    }
+                }
+            }
+        })
+    }, [prefersDarkMode]);
+    return <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+    </ThemeProvider>
+}
