70f342fef6cb2438765558836c99234692d8a252
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-04 14:02:05 -0500

    wip audit logs

--- src/staff/lib/mongo/plugins/audit.ts
+++ src/staff/lib/mongo/plugins/audit.ts
@@ -1,10 +1,47 @@
-import { Schema } from "mongoose";
+import { HydratedDocument, Model, model, models, Schema } from "mongoose";
 
-export interface AuditLogData {
-    created: any
-    updated: any
+export interface AuditData {
+    logs?: AuditLogData[] | string[];
 }
 
 export function AuditLogPlugin(schema: Schema, options) {
-    
+    schema.add({
+        logs: [{
+            type: Schema.Types.ObjectId,
+            ref: 'AuditLog'
+        }]
+    })
 }
+
+
+
+
+import { UserData, UserModel } from '../schema/user';
+import { TimestampData, TimestampOptions, TimestampPlugin } from "./timestamped";
+interface AuditLogData
+extends TimestampData {
+    user: UserData | string;
+}
+
+const schema = new Schema<AuditLogData>({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: UserModel.modelName,
+    },
+}, {
+    ...TimestampOptions
+})
+
+interface AuditLogSchema
+extends AuditLogData {}
+
+schema.plugin(TimestampPlugin);
+
+interface QueryHelpers {
+
+}
+
+
+export type AuditLogDocument = HydratedDocument<AuditLogSchema>;
+export const AuditLogModel = models?.['AuditLog'] || model<AuditLogSchema, Model<AuditLogSchema, QueryHelpers>>('AuditLog', schema);
+
