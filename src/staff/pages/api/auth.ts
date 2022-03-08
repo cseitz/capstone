c2eca1c6a660c6b211afd8f8c371ac3573fc26ed
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-08 16:31:16 -0500

    wip auth

--- src/staff/pages/api/auth.ts
+++ src/staff/pages/api/auth.ts
@@ -1,16 +1,38 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { UserModel } from "lib/mongo/schema/user";
+import { compare } from 'bcrypt';
 
-export interface AuthenticationResponse {
+
+
+export type AuthenticationResponse = 
+    AuthenticationStatusResponse 
+    | AuthenticationLoginResponse 
+    | AuthenticationLogoutResponse;
+
+export interface AuthenticationStatusResponse {
 
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { method } = req;
+export interface AuthenticationLoginResponse {
+    
+}
+
+export interface AuthenticationLogoutResponse {
+    
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<AuthenticationResponse>) {
+    const { method, headers: { authorization } } = req;
     if (method == 'GET') { // return auth status
         
     } else if (method == 'POST') { // login
+        const { body: { email, username, password } } = req;
+        const user = await UserModel.findOne(email ? { email } : { username });
+        if (user) {
+            if (await compare(password, user.password)) {
 
+            }
+        }
     } else if (method == 'DELETE') { // logout
         
     }
