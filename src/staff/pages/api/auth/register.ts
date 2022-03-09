d0915e192dadffead42891e435ab2e04c23c5495
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-09 18:38:08 -0500

    register cleanup

--- src/staff/pages/api/auth/register.ts
+++ src/staff/pages/api/auth/register.ts
@@ -8,14 +8,6 @@ export interface AuthenticationRegisterResponse extends RouteResponse {
     token: string;
 }
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse<AuthenticationRegisterResponse>) {
-//     const { method } = req;
-//     if (method != 'POST') 
-//         return res.status(405).send('Method Not Allowed');
-//     return res.status(500).send('Not Yet Implemented');
-//     //throw new Error('Register - Not Yet Implemented');
-// }
-
 export default Route<AuthenticationRegisterResponse>(async (req, res) => {
     const { method } = req;
     if (method != 'POST')
@@ -34,7 +26,6 @@ export default Route<AuthenticationRegisterResponse>(async (req, res) => {
     })
     await user.save();
     const token = createToken(user);
-    // throw new StatusError(500, 'Not Yet Implemented: ' + JSON.stringify(req.body));
     return res.setHeader('Set-Cookie', serialize('auth', token, { path: '/' })).json({
         token,
     })
