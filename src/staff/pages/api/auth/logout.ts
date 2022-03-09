95c607331241b3b13f74e632f42a48b5eb5b36fd
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-09 18:37:28 -0500

    working logout

--- src/staff/pages/api/auth/logout.ts
+++ src/staff/pages/api/auth/logout.ts
@@ -1,9 +1,17 @@
+import { serialize } from "cookie";
+import { Route, RouteResponse, StatusError } from "lib/route";
 import { NextApiRequest, NextApiResponse } from "next";
+import { verifyToken } from ".";
 
-export interface AuthenticationLogoutResponse {
+export interface AuthenticationLogoutResponse extends RouteResponse {
 
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<AuthenticationLogoutResponse>) {
-    
-}
+export default Route<AuthenticationLogoutResponse>(async (req, res) => {
+    const token = await verifyToken(req);
+    if (token)
+        return res.setHeader('Set-Cookie', serialize('auth', '', { path: '/', maxAge: -1, })).json({
+            
+        });
+    throw new StatusError(401, 'Not Authenticated');
+});
