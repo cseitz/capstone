62774b84dca6348f1a4e40c3ed156a622476fd7d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-09 15:46:13 -0500

    wip register

--- src/staff/pages/api/auth/register.ts
+++ src/staff/pages/api/auth/register.ts
@@ -4,10 +4,44 @@ export interface AuthenticationRegisterResponse {
 
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<AuthenticationRegisterResponse>) {
+// export default async function handler(req: NextApiRequest, res: NextApiResponse<AuthenticationRegisterResponse>) {
+//     const { method } = req;
+//     if (method != 'POST') 
+//         return res.status(405).send('Method Not Allowed');
+//     return res.status(500).send('Not Yet Implemented');
+//     //throw new Error('Register - Not Yet Implemented');
+// }
+
+export default Route(async (req, res) => {
     const { method } = req;
-    if (method != 'POST') 
-        return res.status(405).send('Method Not Allowed');
-    return res.status(500).send('Not Yet Implemented');
-    //throw new Error('Register - Not Yet Implemented');
+    if (method != 'POST') throw new StatusError(405, 'Method Not Allowed');
+    throw new StatusError(500, 'Not Yet Implemented');
+})
+
+class StatusError extends Error {
+    statusCode: number;
+    constructor(statusCode: number, msg: string) {
+        super(msg);
+        this.statusCode = statusCode;
+    }
+}
+
+function Route<T>(handler: (req: NextApiRequest, res: NextApiResponse<T>) => any) {
+    return function(req, res) {
+        try {
+            Promise.resolve(handler(req, res)).catch(err => {
+                if (err instanceof StatusError) {
+                    res.status(err.statusCode).send(err.message);
+                } else {
+                    throw err;
+                }
+            })
+        } catch(err) {
+            if (err instanceof StatusError) {
+                res.status(err.statusCode).send(err.message);
+            } else {
+                throw err;
+            }
+        }
+    }
 }
