cb1d3fc6f7db519d25a357f445969d4454c4d080
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-10 17:25:42 -0500

    fixed navbar

---
+++ src/staff/pages/_app.tsx
@@ -0,0 +1,33 @@
+import { createTheme, CssBaseline, useMediaQuery, ThemeProvider } from "@mui/material";
+import { AppProps } from "next/app";
+import { useMemo } from "react";
+import { NavBar } from "../ui/components/navbar";
+import '../ui/styles/global.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+    const prefersDarkMode = useMediaQuery(`(prefers-color-scheme: dark)`);
+    const theme = useMemo(() => {
+        return createTheme({
+            palette: {
+                mode: prefersDarkMode ? 'dark' : 'light',
+                background: {
+                    default: prefersDarkMode ? '#35363a' : 'white',
+                }
+            },
+            components: {
+                MuiPagination: {
+                    styleOverrides: {
+                        ul: {
+                            justifyContent: 'center'
+                        }
+                    }
+                }
+            }
+        })
+    }, [prefersDarkMode]);
+    return <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar />
+        <Component {...pageProps} />
+    </ThemeProvider>
+}
