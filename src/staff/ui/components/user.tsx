3f55bc58b91d22ae51c3bbac757ffbe1e4edef1f
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-14 14:58:37 -0400

    user card header

--- src/staff/ui/components/user.tsx
+++ src/staff/ui/components/user.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, CardHeader, CardProps, Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, CardProps, Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Box } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useState } from "react";
@@ -17,14 +17,23 @@ function UserCard(props: {
         username: 'cseitz',
         info: {
             firstName: 'Chris',
-            lastName: 'Seitz'
+            lastName: ' Seitz'
         },
         created: new Date('2022-03-10T19:17:40.571+00:00'),
         updated: new Date('2022-03-10T19:17:40.571+00:00'),
 
     };
+    const hasName = Boolean(user?.info?.firstName?.trim() && user?.info?.lastName?.trim());
+    const hasEmail = Boolean(user?.email.trim());
     return <Card {...cardProps}>
-        <CardHeader title={"Chris Seitz"} subheader={"cseitz5@kent.edu"} action={<IconButton>
+        <CardHeader {...{
+            title: <Box component="span" sx={{ color: !hasName && 'error.main' }}>
+                {hasName ? [user.info.firstName, user.info.lastName].map(o => o.trim()).join(' ') : 'Missing Name'}
+            </Box>,
+            subheader: <Box component="span" sx={{ color: !hasEmail && 'error.main' }}>
+                {hasEmail ? user.email : 'No Email'}
+            </Box>
+        }} action={<IconButton>
             <MoreVertIcon />
         </IconButton>} />
         <CardContent>
