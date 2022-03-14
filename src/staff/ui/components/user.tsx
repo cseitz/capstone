b1884428eb7340be648e22eadada46a5c4c6df1d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-14 15:30:17 -0400

    wip user card

--- src/staff/ui/components/user.tsx
+++ src/staff/ui/components/user.tsx
@@ -1,7 +1,9 @@
-import { Button, Card, CardActions, CardContent, CardHeader, CardProps, Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Box } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, CardProps, Checkbox, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Box, Select, MenuItem, Grid } from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { useState } from "react";
+import TodayIcon from '@mui/icons-material/Today';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import { useState, useMemo } from "react";
 import type { UserData } from "lib/mongo/schema/user";
 
 
@@ -10,7 +12,7 @@ function UserCard(props: {
 } & CardProps) {
     const { id, ...cardProps } = props;
     const [mode, setMode] = useState<'view' | 'edit'>('view');
-    const user: UserData = {
+    const user: UserData = useMemo(() => ({
         email: "cseitz5@kent.edu",
         role: 'pending',
         password: '',
@@ -22,7 +24,7 @@ function UserCard(props: {
         created: new Date('2022-03-10T19:17:40.571+00:00'),
         updated: new Date('2022-03-10T19:17:40.571+00:00'),
 
-    };
+    }), []);
     const hasName = Boolean(user?.info?.firstName?.trim() && user?.info?.lastName?.trim());
     const hasEmail = Boolean(user?.email.trim());
     return <Card {...cardProps}>
@@ -37,7 +39,42 @@ function UserCard(props: {
             <MoreVertIcon />
         </IconButton>} />
         <CardContent>
-            hi there
+            <Typography variant="h6">Details</Typography>
+            <Grid container columns={12} spacing={2}>
+                <Grid item xs={6}>
+                    <Typography>what</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography>test</Typography>
+                </Grid>
+            </Grid>
+            <List subheader={"Details"} dense>
+                <ListItem>
+                    <ListItemIcon>
+                        <TodayIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Registered" secondary={(user.created as Date).toLocaleString('en-us', {
+                        dateStyle: 'short',
+                        timeStyle: 'short'
+                    })} />
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon>
+                        <ScheduleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Updated" secondary={(user.updated as Date).toLocaleString('en-us', {
+                        dateStyle: 'short',
+                        timeStyle: 'short'
+                    })} />
+                </ListItem>
+            </List>
+            <Select value={user.role} onChange={({ target }) => { user.role = target.value as any; }}>
+                <MenuItem value={'pending'}>Pending</MenuItem>
+                <MenuItem value={'banned'}>Banned</MenuItem>
+                <MenuItem value={'user'}>User</MenuItem>
+                <MenuItem value={'staff'}>Staff</MenuItem>
+                <MenuItem value={'admin'}>Admin</MenuItem>
+            </Select>
         </CardContent>
         <CardActions>
             <Button>what</Button>
