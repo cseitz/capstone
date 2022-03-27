0cfa0664de2926f34c1bb73595b6c286ccd2f756
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-27 17:21:45 -0400

    user patching

--- src/staff/pages/api/users/[id].ts
+++ src/staff/pages/api/users/[id].ts
@@ -1,5 +1,6 @@
 import { isAuthenticated } from "lib/auth";
 import { UserData, UserDocument, UserModel } from "lib/mongo/schema/user";
+import { UpdateDocument } from "lib/mongo/utils";
 import { Route, StatusError } from "lib/route";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -12,9 +13,27 @@ const isStaff = isAuthenticated({
 })
 
 export default Route<UserResponse>(async (req, res) => {
-    if (!isStaff(req)) throw new StatusError(403, 'Unauthorized');
+    const { method, headers, query } = req;
+    const client = isStaff(req);
+    if (!client) throw new StatusError(403, 'Unauthorized');
     const user = await UserModel.findById(req.query.id);
-    res.json({
-        user
-    })
+    if (method == 'GET') {
+        return res.json({
+            user
+        })
+    } else if (method == 'PATCH') {
+        await user.audit({
+            user: client.id,
+        })
+        UpdateDocument(user, req.body);
+        await user.commitWith(req, {
+            action: 'Updated User',
+        });
+        await user.save();
+        return res.json({
+            user
+        })
+    }
+}, {
+    methods: ['GET', 'PATCH']
 });
