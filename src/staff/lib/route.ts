51c9b6344b36ee3c29da8542cf767c8025a4b080
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-03-27 16:48:38 -0400

    route allowed methods

--- src/staff/lib/route.ts
+++ src/staff/lib/route.ts
@@ -15,9 +15,19 @@ export class StatusError extends Error {
     }
 }
 
-export function Route<T>(handler: (req: NextApiRequest, res: NextApiResponse<T | RouteResponse>) => any) {
+export interface RouteConfig {
+    methods?: ('GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'PATCH')[];
+}
+
+export function Route<T>(handler: (req: NextApiRequest, res: NextApiResponse<T | RouteResponse>) => any, config?: RouteConfig) {
     return function (req, res) {
         try {
+            if (config) {
+                if ('methods' in config) {
+                    if (!config.methods.includes(req.method))
+                        throw new StatusError(405, 'Method Not Allowed');
+                }
+            }
             Promise.resolve(handler(req, res)).catch(err => {
                 if (err instanceof StatusError) {
                     res.status(err.statusCode).json({ error: err.message, ...err?.data });
