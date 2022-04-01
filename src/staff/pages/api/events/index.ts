bdddffae17fa787a8c361b976d26840bb0b47e83
Nolan Spencer <nspencer3513@gmail.com>
2022-04-01 14:23:41 -0400

    Event API

---
+++ src/staff/pages/api/events/index.ts
@@ -0,0 +1,22 @@
+import { isAuthenticated } from "lib/auth";
+import { EventDocument, EventModel, EventData } from "lib/mongo/schema/event";
+import { Route, StatusError } from "lib/route";
+import { NextApiRequest, NextApiResponse } from "next";
+
+//Exports list of tickets
+export interface EventListResponse {
+    events: EventDocument[]
+}
+
+//makes sure the request is coming from an authenticated user
+const isStaff = isAuthenticated({
+    role: ['pending', 'user', 'staff', 'admin']
+})
+
+export default Route<EventListResponse>(async (req, res) => {
+    if (!isStaff(req)) throw new StatusError(403, 'Unauthorized');
+    const events = await EventModel.find()
+    res.json({
+        events
+    })
+});
