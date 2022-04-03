7e52230dd74903f4beb7943a57f6f83b052e521c
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-03 17:13:33 -0400

    reset form button and state persistence

--- src/app/pages/register.tsx
+++ src/app/pages/register.tsx
@@ -1,6 +1,6 @@
 import { Button, CircularProgress, Grid, Paper, Step, StepContent, StepLabel, Stepper, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { LoginRegisterContainer } from "./login";
 
 
@@ -10,17 +10,43 @@ const steps: {
     content: (step: (typeof steps[number]) & { index: number }) => JSX.Element
 }[] = [];
 
+
+const storage = typeof window == 'undefined' ? {
+    getItem(key: string) { return null },
+    setItem(key: string, value: any) { },
+    removeItem(key: string) { },
+} : localStorage;
+function useLocalState<T = string>(key: string, defaultValue: T, caster: (value: any) => T = (value: any) => value) {
+    const [value, setValue] = useState<T>(caster(storage.getItem(key) as any || defaultValue));
+    return [
+        value,
+        ((value: Parameters<typeof setValue>[0]) => {
+            setValue(value);
+            storage.setItem(key, value as any);
+            return;
+        }) as typeof setValue,
+    ] as [T, typeof setValue];
+}
+
 export default function RegisterPage() {
     const [activeStep, setActiveStep] = useState(0);
+    const showReset = Object.keys(storage).find(o => o.includes('register.'));
+    const resetForm = useCallback(function (reload: boolean = true) {
+        const keys = Object.keys(storage).filter(o => o.includes('register.'));
+        for (const key of keys) {
+            storage.removeItem(key);
+        }
+        if (reload) location.reload();
+    }, []);
     return <Box sx={{ mb: 3, mt: 10 }}>
-        <Box sx={{ margin: 'auto', width: 'min(500px, 90vw)' }}>
+        <Box sx={{ margin: 'auto', width: 'min(500px, 90vw)', textAlign: 'center' }}>
             <Typography variant="h3" style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', marginTop: 25, marginBottom: 25 }}>
                 Register
             </Typography>
             <Stepper activeStep={activeStep} orientation="vertical">
                 {steps.map((S, index) => {
                     const isLast = index == steps.length - 1;
-                    return <Step key={index}>
+                    return <Step key={index} sx={{ pr: '16px' }}>
                         <StepLabel optional={S?.optional}>
                             {S.title}
                         </StepLabel>
@@ -43,6 +69,11 @@ export default function RegisterPage() {
                     </Box>
                 ) : ''}
             </Stepper>
+            {showReset && activeStep != steps.length - 0 ? <>
+                <Button onClick={() => resetForm(true)} sx={{ mt: 2, mb: 1 }}>
+                    Reset Form
+                </Button>
+            </> : ''}
         </Box>
     </Box>
 }
@@ -50,11 +81,11 @@ export default function RegisterPage() {
 steps.push({
     title: 'Account',
     content: (step) => {
-        const [firstName, setFirstName] = useState<string>('');
-        const [lastName, setLastName] = useState<string>('');
-        const [email, setEmail] = useState<string>('');
-        const [password, setPassword] = useState<string>('');
-        const [confirmPassword, setConfirmPassword] = useState<string>('');
+        const [firstName, setFirstName] = useLocalState<string>('register.firstName', '');
+        const [lastName, setLastName] = useLocalState<string>('register.lastName', '');
+        const [email, setEmail] = useLocalState<string>('register.email', '');
+        const [password, setPassword] = useLocalState<string>('register.password', '');
+        const [confirmPassword, setConfirmPassword] = useLocalState<string>('register.confirmPassword', '');
         return <>
             <Typography variant="h5">Account Registration</Typography>
             <Typography>Please fill out the following fields to register for an account.</Typography>
@@ -83,18 +114,24 @@ steps.push({
 
 steps.push({
     title: 'Demographics',
-    content: (step) => <>
-        <Typography variant="h5">Demographics</Typography>
-        <Typography>Please fill out the following fields.</Typography>
-    </>
+    content: (step) => {
+
+        return <>
+            <Typography variant="h5">Demographics</Typography>
+            <Typography>Please fill out the following fields.</Typography>
+        </>
+    }
 })
 
 steps.push({
     title: 'Preferences',
-    content: (step) => <>
-        <Typography variant="h5">Preferences</Typography>
-        <Typography>Please fill out the following fields.</Typography>
-    </>
+    content: (step) => {
+
+        return <>
+            <Typography variant="h5">Preferences</Typography>
+            <Typography>Please fill out the following fields.</Typography>
+        </>
+    }
 })
 
 steps.push({
