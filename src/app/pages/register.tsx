f3835383021c4653edfaf3b634cbb5266dcea485
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-03 18:27:57 -0400

    wip validation

--- src/app/pages/register.tsx
+++ src/app/pages/register.tsx
@@ -1,6 +1,6 @@
-import { Button, CircularProgress, Grid, Paper, Step, StepContent, StepLabel, Stepper, TextField, Typography } from "@mui/material";
+import { Button, CircularProgress, FormControl, FormHelperText, FormLabel, Grid, Paper, RadioGroup, Step, StepContent, StepLabel, Stepper, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { useCallback, useState } from "react";
+import { cloneElement, createContext, useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
 
 
 const steps: {
@@ -27,16 +27,55 @@ function useLocalState<T = string>(key: string, defaultValue: T, caster: (value:
     ] as [T, typeof setValue];
 }
 
+
+const RegisterContext = createContext<ReturnType<typeof RegisterForm>>(RegisterForm());
+
+type RegistrationData = {
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    confirmPassword: string;
+};
+
+function RegisterForm() {
+    const data: RegistrationData = {
+        email: '',
+        firstName: '',
+        lastName: '',
+        password: '',
+        confirmPassword: '',
+    }
+    return {
+        data,
+        useState<T = string>(key: keyof RegistrationData) {
+            const [value, setValue] = useLocalState<T>('register.' + key, this.data[key]);
+            useEffect(() => {
+                this.data[key] = value;
+            }, [value]);
+            return [value, setValue] as [T, typeof setValue]
+        },
+        get showReset() {
+            return Object.keys(storage).find(o => o.includes('register.'))
+        },
+        reset(reload = true) {
+            const keys = Object.keys(storage).filter(o => o.includes('register.'));
+            for (const key of keys) {
+                storage.removeItem(key);
+            }
+            if (reload) location.reload();
+        },
+        submit() {
+            console.log(this.data);
+            this.reset(false);
+        }
+    }
+}
+
 export default function RegisterPage() {
     const [activeStep, setActiveStep] = useState(0);
-    const showReset = Object.keys(storage).find(o => o.includes('register.'));
-    const resetForm = useCallback(function (reload: boolean = true) {
-        const keys = Object.keys(storage).filter(o => o.includes('register.'));
-        for (const key of keys) {
-            storage.removeItem(key);
-        }
-        if (reload) location.reload();
-    }, []);
+    const form = useContext(RegisterContext);
+    const { showReset } = form;
     return <Box sx={{ mb: 3, mt: 10 }}>
         <Box sx={{ margin: 'auto', width: 'min(500px, 90vw)', textAlign: 'center' }}>
             <Typography variant="h3" style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', marginTop: 25, marginBottom: 25 }}>
@@ -45,14 +84,14 @@ export default function RegisterPage() {
             <Stepper activeStep={activeStep} orientation="vertical">
                 {steps.map((S, index) => {
                     const isLast = index == steps.length - 1;
-                    return <Step key={index} sx={{ pr: '16px' }}>
+                    return <Step sx={{ pr: '16px' }} key={index}>
                         <StepLabel optional={S?.optional}>
                             {S.title}
                         </StepLabel>
                         <StepContent>
                             {S.content({ ...S, index })}
                             <Box sx={isLast ? { maxWidth: '300px', mx: 'auto', mt: 2 } : { mt: 2 }}>
-                                <Button variant="contained" onClick={() => setActiveStep(activeStep + 1)} sx={{ mt: 1, float: 'right' }} fullWidth={isLast}>
+                                <Button variant="contained" onClick={() => activeStep == steps.length - 1 ? form.submit() : setActiveStep(activeStep + 1)} sx={{ mt: 1, float: 'right' }} fullWidth={isLast}>
                                     {isLast ? 'Register' : 'Continue'}
                                 </Button>
                                 <Button variant="text" onClick={() => setActiveStep(activeStep - 1)} disabled={index === 0} sx={{ mt: 1, float: 'left' }} fullWidth={isLast}>
@@ -69,7 +108,7 @@ export default function RegisterPage() {
                 ) : ''}
             </Stepper>
             {showReset && activeStep != steps.length - 0 ? <>
-                <Button onClick={() => resetForm(true)} sx={{ mt: 2, mb: 1 }}>
+                <Button onClick={() => form.reset(true)} sx={{ mt: 2, mb: 1 }}>
                     Reset Form
                 </Button>
             </> : ''}
@@ -77,21 +116,93 @@ export default function RegisterPage() {
     </Box>
 }
 
+
+const REQUIRED_TEXT = 'This field is required!';
+function ValidatedInput(props: {
+    children: any,
+    label?: string,
+    value?: any,
+    onChange?: any,
+    validate?: (value: any) => string,
+    required?: boolean,
+    enforced?: boolean,
+}) {
+    let { children, label, value, onChange, validate, required, enforced } = props;
+    if (required) {
+        validate = (value: string) => value ? (props.validate ? props.validate(value) : undefined) : REQUIRED_TEXT;
+    }
+    if (onChange) onChange = props.onChange ? ({ target }) => props.onChange((target as any).value) : undefined;
+    const [touched, setTouched] = useState(enforced);
+    if (enforced && !touched) setTouched(true);
+    let error = useMemo(() => validate(value), [value]);
+    if (!touched) error = undefined;
+    const isValid = !error || !touched;
+    useEffect(() => {
+        console.log('changed', value);
+        if (!touched && value) setTouched(true);
+    }, [value]);
+    const passed = {
+        label,
+        placeholder: label,
+        onChange,
+        value,
+        error: !isValid,
+        helperText: error
+    }
+    const failed = {}
+    for (const key in passed) {
+        if (!children?.type?.propTypes?.[key]) {
+            failed[key] = passed[key];
+            delete passed[key];
+        }
+    }
+    console.log({ failed })
+    const element = cloneElement(children, passed);
+    return <FormControl error={Boolean(error)}>
+        {label && failed?.['label'] ? <FormLabel>{label}</FormLabel> : ''}
+        {element}
+        {error && failed?.['helperText'] ? <FormHelperText>{error}</FormHelperText> : ''}
+    </FormControl>
+}
+
+
 steps.push({
     title: 'Account',
     content: (step) => {
-        const [firstName, setFirstName] = useLocalState<string>('register.firstName', '');
-        const [lastName, setLastName] = useLocalState<string>('register.lastName', '');
-        const [email, setEmail] = useLocalState<string>('register.email', '');
-        const [password, setPassword] = useLocalState<string>('register.password', '');
-        const [confirmPassword, setConfirmPassword] = useLocalState<string>('register.confirmPassword', '');
+        const form = useContext(RegisterContext);
+        const [firstName, setFirstName] = form.useState('firstName');
+        const [lastName, setLastName] = form.useState('lastName');
+        const [email, setEmail] = form.useState('email');
+        const [password, setPassword] = form.useState('password');
+        const [confirmPassword, setConfirmPassword] = form.useState('confirmPassword');
+
+        const [issues, setIssues] = useState<any>({});
+        const [isValid, setIsValid] = useState(false);
+        const validate = useCallback(() => {
+            const issues = {};
+            if (password != confirmPassword) {
+                issues['confirmPassword'] = 'Password does not match!'
+            }
+            if (!firstName) issues['firstName'] = REQUIRED_TEXT;
+            if (!lastName) issues['lastName'] = REQUIRED_TEXT;
+            if (!email) issues['email'] = REQUIRED_TEXT;
+            else if (!/\S+@\S+\.\S+/.test(email)) issues['email'] = 'Not a valid email!';
+            setIssues(issues);
+            setIsValid(Object.keys(issues).length == 0);
+            return issues;
+        }, [firstName, lastName, email, password, confirmPassword]);
+
         return <>
             <Typography variant="h5">Account Registration</Typography>
             <Typography>Please fill out the following fields to register for an account.</Typography>
             <br />
             <Grid container spacing={2}>
                 <Grid item xs={6}>
-                    <TextField label="First Name" type="text" placeholder="First Name" value={firstName} onChange={({ target: { value } }) => setFirstName(value)} fullWidth />
+                    {/* <TextField label="First Name" type="text" placeholder="First Name" value={firstName} onChange={({ target: { value } }) => setFirstName(value)} onBlur={() => firstName && validate()} fullWidth /> */}
+                    <ValidatedInput label="First Name" onChange={setFirstName} value={firstName} required>
+                        <TextField type="text" fullWidth />
+                        {/* <RadioGroup /> */}
+                    </ValidatedInput>
                 </Grid>
                 <Grid item xs={6}>
                     <TextField label="Last Name" type="text" placeholder="Last Name" value={lastName} onChange={({ target: { value } }) => setLastName(value)} fullWidth />
