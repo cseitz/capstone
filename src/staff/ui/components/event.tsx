455de314c864744225e9b6e64e2e54328a50765f
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-05 19:37:32 -0400

    very nice staff event modal

--- src/staff/ui/components/event.tsx
+++ src/staff/ui/components/event.tsx
@@ -1,13 +1,15 @@
-import { Box, Card, CardHeader, CardActions, CardProps, Checkbox, IconButton, List, ListItem, ListItemButton, ListItemText, Modal, Typography, CardContent, Button, ListItemIcon, TextField } from "@mui/material";
+import { Box, Card, CardHeader, CardActions, CardProps, Checkbox, IconButton, List, ListItem, ListItemButton, ListItemText, Modal, Typography, CardContent, Button, ListItemIcon, TextField, Grid, Tooltip, Divider } from "@mui/material";
 import { EventListResponse } from "pages/api/events";
 import { EventResponse } from "pages/api/events/[id]";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { useAlert } from "./alert";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import TodayIcon from '@mui/icons-material/Today';
 import ScheduleIcon from '@mui/icons-material/Schedule';
 import PeopleIcon from '@mui/icons-material/People';
+import DeleteIcon from '@mui/icons-material/Delete'
+import CloseIcon from '@mui/icons-material/Close'
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DateTimePicker from '@mui/lab/DateTimePicker';
@@ -15,8 +17,9 @@ import type { EventData } from "lib/mongo/schema/event";
 
 export function EventCard(props: {
     event: string;
+    exit?: () => void;
 } & CardProps) {
-    const { event: id, ...cardProps } = props;
+    const { event: id, exit = () => { }, ...cardProps } = props;
     const isCreate = id == 'create';
     const [mode, setMode] = useState<'view' | 'edit'>(isCreate ? 'edit' : 'view');
     const isEditing = mode == 'edit';
@@ -33,49 +36,159 @@ export function EventCard(props: {
     const { event } = !isCreate ? (data || {}) : { event: creating };
     const hasType = Boolean(event?.type);
     const alert = useAlert();
+
+    const [title, setTitle] = useState<string>(null);
+    const [type, setType] = useState<string>(null);
+    const [description, setDescription] = useState<string>(null);
+    const [startsAt, setStartsAt] = useState<Date | null>(null);
+    const [endsAt, setEndsAt] = useState<Date | null>(null);
+    const discardChanges = useCallback(function () {
+        setTitle(event.title);
+        setType(event.type);
+        setDescription(event.description);
+        setStartsAt(event?.startsAt ? new Date(event?.startsAt) : null);
+        setEndsAt(event?.endsAt ? new Date(event?.endsAt) : null);
+        setMode('view');
+    }, [event]);
+
     useEffect(() => {
-        if (!isLoading) { return; }
+        if (isLoading || isCreate) { return; }
         alert.success({
-            message: 'Loaded Event ' + event?.title || "[Missing title]",
+            message: 'Loaded ' + (event?.title || "[Missing title]"),
             duration: 2000,
         })
+        setTitle(event?.title || '');
+        setType(event?.type || '');
+        setDescription(event?.description || '');
+        setStartsAt(event?.startsAt ? new Date(event?.startsAt) : null);
+        setEndsAt(event?.endsAt ? new Date(event?.endsAt) : null)
     }, [isLoading]);
-    if (isLoading) return <Card {...cardProps} />;
-    const [startsAt, setStartsAt] = useState(event?.startsAt);
+
+    const topActions = <>
+        {!isCreate && <Tooltip title="Delete" placement="left" disableInteractive>
+            <IconButton onClick={() => alert.error('Not Yet Implemented (Delete)', { duration: 1000 })}>
+                <DeleteIcon />
+            </IconButton>
+        </Tooltip>}
+        <Tooltip title="Close" placement="left" disableInteractive>
+            <IconButton onClick={exit}>
+                <CloseIcon />
+            </IconButton>
+        </Tooltip>
+    </>
+
+    // if (isLoading) return <Card {...cardProps} />;
     return <>
         <Card {...cardProps}>
+
             <CardHeader {...{
                 title: !isCreate ? (<Box component="span" sx={{}}>
-                    {event.title} {event.type && <>
+                    {event?.title} {event?.type && <>
                         - <Typography component="span" sx={{}}>
-                            {event.type}
+                            {event?.type}
                         </Typography>
                     </>}
                 </Box>) : 'Create Event',
                 subheader: !isCreate && <Box component="span" sx={{}}>
-                    {new Date(event.startsAt).toLocaleString('en-us', {
+                    {new Date(event?.startsAt).toLocaleString('en-us', {
                         dateStyle: 'short',
                         timeStyle: 'short'
-                    }) + " - " + new Date(event.endsAt).toLocaleString('en-us', {
+                    }) + " - " + new Date(event?.endsAt).toLocaleString('en-us', {
                         dateStyle: 'short',
                         timeStyle: 'short'
                     })}</Box>
-            }} action={<IconButton>
-                <MoreVertIcon />
-            </IconButton>} />
+            }} action={topActions} />
+
             <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <CardContent>
-                    <DateTimePicker renderInput={(props) => <TextField {...props} />} value={startsAt} onChange={setStartsAt} />
+                    <Grid container spacing={2}>
+                        {isEditing && <>
+                            <Grid item xs={8}>
+                                <TextField label="Title" placeholder="Title" fullWidth value={title} onChange={({ target: { value } }) => setTitle(value)} />
+                            </Grid>
+
+                            <Grid item xs={4}>
+                                <TextField label="Type" placeholder="Type" fullWidth value={type} onChange={({ target: { value } }) => setType(value)} />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField label="Description" placeholder="Description" fullWidth multiline minRows={2} value={description} onChange={({ target: { value } }) => setDescription(value)} />
+                            </Grid>
+
+                            <Grid item xs={6}>
+                                <DateTimePicker label="Starts" renderInput={(props) => <TextField fullWidth {...props} />} value={startsAt} onChange={val => setStartsAt(val)} />
+                            </Grid>
+
+                            <Grid item xs={6}>
+                                <DateTimePicker label="Ends" renderInput={(props) => <TextField fullWidth {...props} />} value={endsAt} onChange={val => setEndsAt(val)} />
+                            </Grid>
+                        </>}
+
+                        {isViewing && <>
+                            <Grid item xs={12}>
+                                <Typography variant="body1">
+                                    {description || "No Description"}
+                                </Typography>
+                            </Grid>
+
+                            <Grid item xs={12}>
+
+                            </Grid>
+
+                            <Grid item xs={1}>
+                                <Tooltip title="Created" disableInteractive>
+                                    <TodayIcon />
+                                </Tooltip>
+                            </Grid>
+
+
+
+                            <Grid item xs={5}>
+                                <Tooltip title="Created" disableInteractive>
+                                    <Typography component="span">
+                                        {new Date(event?.created).toLocaleString('en-us', {
+                                            dateStyle: 'short',
+                                            timeStyle: 'short'
+                                        })}
+                                    </Typography>
+                                </Tooltip>
+                            </Grid>
+
+                            <Grid item xs={1}>
+                                <Tooltip title="Updated" disableInteractive>
+                                    <ScheduleIcon />
+                                </Tooltip>
+                            </Grid>
+                            <Grid item xs={5}>
+                                <Tooltip title="Updated" disableInteractive>
+                                    <Typography component="span">
+                                        {new Date(event?.updated).toLocaleString('en-us', {
+                                            dateStyle: 'short',
+                                            timeStyle: 'short'
+                                        })}
+                                    </Typography>
+                                </Tooltip>
+                            </Grid>
+                        </>}
+
+                    </Grid>
+
                 </CardContent>
+
             </LocalizationProvider>
-            <CardActions>
+
+            <CardActions sx={{ justifyContent: 'space-between' }}>
                 {isViewing && <Button onClick={() => setMode('edit')}>Edit</Button>}
-                {isEditing && !isCreate && <Button onClick={() => setMode('view')}>Discard Changes</Button>}
+                {isEditing && !isCreate && <>
+                    <Button onClick={() => alert.error('Not Yet Implemented (Save)', { duration: 1000 })}>Save Changes</Button>
+                    <Button onClick={discardChanges}>Discard Changes</Button>
+                </>}
                 {isEditing && isCreate && <>
-                    <Button onClick={() => alert('Not Yet Implemented')}>Create Event</Button>
-                    <Button onClick={() => alert('Not Yet Implemented')}>Cancel</Button>
+                    <Button onClick={() => alert.error('Not Yet Implemented (Create)', { duration: 1000 })}>Create Event</Button>
+                    <Button onClick={exit}>Cancel</Button>
                 </>}
             </CardActions>
+
         </Card>
     </>
 }
@@ -144,11 +257,12 @@ function EventListComponent(props: { showCreate?: boolean, onClose?: () => void 
                 unique: 'eventListRefreshedAt'
             });
         firstLoad.current = false;
-    }, [dataUpdatedAt])
+    }, [dataUpdatedAt]);
+    const exit = () => { setOpen(null); onCloseListener() };
     return <>
-        <Modal open={Boolean(open)} onClose={() => { setOpen(null); onCloseListener() }}>
+        <Modal open={Boolean(open)} onClose={exit}>
             <Box sx={{ width: '100vw', maxWidth: 600, mx: 'auto', mt: '10vh' }}>
-                {open && <EventCard event={open} />}
+                {open && <EventCard event={open} exit={exit} />}
             </Box>
         </Modal>
         <List>
