8e7a50be93206f52a05bba8874a9598522ec6c8b
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-05 18:30:54 -0400

    hid extra message

--- src/staff/pages/events.tsx
+++ src/staff/pages/events.tsx
@@ -35,7 +35,6 @@ export default function EventPage() {
                 Create Event
             </Button>
         </Box>
-        <Typography>hi</Typography>
 
         <EventList showCreate={showCreate} onClose={() => setShowCreate(false)} />
     </Box>
