a9353bb811f7ad3f0607e8870e057799ae745e4d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-06 15:34:31 -0400

    added update functionality to ticket endpoint

--- src/staff/pages/api/tickets/[id].ts
+++ src/staff/pages/api/tickets/[id].ts
@@ -1,5 +1,6 @@
 import { isAuthenticated } from "lib/auth";
 import { TicketData, TicketModel } from "lib/mongo/schema/ticket";
+import { UpdateDocument } from "lib/mongo/utils";
 import { Route, StatusError } from "lib/route";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -13,14 +14,30 @@ const isStaff = isAuthenticated({
 })
 
 export default Route<TicketResponse>(async (req, res) => {
-    const user = isStaff(req);
+    const client = isStaff(req);
     const { method, headers, query } = req;
-    if (!user) throw new StatusError(403, 'Unauthorized');
+    if (!client) throw new StatusError(403, 'Unauthorized');
     const ticket = await TicketModel.findById(req.query.id);
-    //ticket.assignee = user.id;
     if (method == 'GET') {
         return res.json({
             ticket
         })
+    } else if (method == 'PATCH') {
+        await ticket.audit({
+            user: client.id,
+        })
+        UpdateDocument(ticket, req.body);
+        if ('status' in req.body) {
+            ticket.assignee = client.id;
+        }
+        await ticket.commitWith(req, {
+            action: 'Updated Ticket',
+        });
+        await ticket.save();
+        return res.json({
+            ticket
+        })
     }
+}, {
+    methods: ['GET', 'PATCH']
 });
