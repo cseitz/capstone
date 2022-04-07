c7d6d8894e100e2041951a6abb576db51a5a16b9
tgargasz314 <78514686+tgargasz314@users.noreply.github.com>
2022-04-06 21:20:07 -0400

    maidens

--- src/staff/pages/api/details/[section].ts
+++ src/staff/pages/api/details/[section].ts
@@ -1,11 +1,22 @@
 //Mockup API for retrieving text
 //nightmarenightmarenightmare
 
+import { UpdateDocument } from "lib/mongo/utils";
 import { NextApiRequest, NextApiResponse } from "next";
 import { FillableTextData } from ".";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { section } = req.query   
+    const {method} = req;
+    const { section } = req.query
+    const doc = (await FillableTextData())?.[section as string]
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json((await FillableTextData())?.[section as string] || {})
+    if (method == "GET") {
+        res.json(doc || {})    
+    }
+    else if(method == "PATCH" && doc) {
+        console.log("Maidens: ", doc);
+        UpdateDocument(doc, req.body);
+        await doc.save();
+        res.json(doc);        
+    }
 }
