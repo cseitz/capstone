1acc614d374a11abf922ca8cd155e5c32c91a542
tgargasz314 <78514686+tgargasz314@users.noreply.github.com>
2022-04-06 21:34:42 -0400

    fuck

--- src/staff/pages/api/details/[section].ts
+++ src/staff/pages/api/details/[section].ts
@@ -8,15 +8,15 @@ import { FillableTextData } from ".";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const {method} = req;
     const { section } = req.query
-    const doc = (await FillableTextData())?.[section as string]
+    const doc = (await FillableTextData())?.[section as string];
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     if (method == "GET") {
         res.json(doc || {})    
-    }
-    else if(method == "PATCH" && doc) {
-        console.log("Maidens: ", doc);
+    } else if(method == "PATCH") {
         UpdateDocument(doc, req.body);
         await doc.save();
         res.json(doc);        
-    }
+    } else if (method == 'OPTIONS') { res.send('ok') }
 }
