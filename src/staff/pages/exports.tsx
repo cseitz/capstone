ea6d3259532cb5885db10229cdd22bb3db304ed2
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-08 00:38:44 -0400

    updated exports page

--- src/staff/pages/exports.tsx
+++ src/staff/pages/exports.tsx
@@ -5,31 +5,42 @@ import { useRouter } from 'next/router';
 import { useAlert } from 'ui/components/alert';
 import Link from 'next/link'
 
+const endpoints: {
+    key: string,
+    title: JSX.Element | string,
+    description: JSX.Element | string,
+}[] = [
+        {
+            key: 'logs',
+            title: 'Audit Logs',
+            description: 'All Audit logs can be downloaded here.',
+        },
+        {
+            key: 'users',
+            title: 'User List',
+            description: 'All Users can be downloaded here.',
+        }
+    ]
 
-export default function BasicButtons() {
-    return(
-        <Box sx={{maxWidth:"800px", mx:"auto"}}>
-            <br />
-            <Box>
-                <Typography variant = "h5">
-                    Audit Logs
-                    <Link href={"/api/exports/logs"}>
-                    <Button variant="contained" sx={{ float: "right"}}>Download</Button>
-                    </Link>
-                </Typography>
-                <Typography> All Audit logs can be downloaded here.</Typography>
-            </Box>
-            <br />
-            <Box>
-                <Typography variant = "h5">
-                    User Logs
-                    <Link href={"/api/exports/users"}>
-                    <Button variant="contained" sx={{ float: "right"}}>Download</Button>
-                    </Link>
-                </Typography>
-                <Typography> All User logs can be downloaded here.</Typography>
-            </Box>
-
+export default function ExportsPage() {
+    return (
+        <Box sx={{ maxWidth: "800px", mx: "auto", p: 2 }}>
+            <Typography variant="h4">Exports</Typography>
+            {endpoints.map(endpoint => {
+                return <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', my: 2 }} key={endpoint.key}>
+                    <Box>
+                        <Typography variant="h5">
+                            {endpoint.title}
+                        </Typography>
+                        <Typography>{endpoint.description}</Typography>
+                    </Box>
+                    <Box sx={{ p: 1 }}>
+                        <Link href={"/api/exports/" + endpoint.key}>
+                            <Button variant="contained">Download</Button>
+                        </Link>
+                    </Box>
+                </Box>
+            })}
         </Box>
     );
 }
