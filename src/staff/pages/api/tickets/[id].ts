ed0b07c8f317084ddb84fe6c51f54635476591a3
Brandon <btcossin@gmail.com>
2022-04-12 22:30:01 -0400

    Commented id.ts

--- src/staff/pages/api/tickets/[id].ts
+++ src/staff/pages/api/tickets/[id].ts
@@ -12,27 +12,33 @@ export interface TicketResponse {
 const isStaff = isAuthenticated({
     role: ['pending', 'user', 'staff', 'admin']
 })
-
+//Returns ticket based off id
 export default Route<TicketResponse>(async (req, res) => {
     const client = isStaff(req);
     const { method, headers, query } = req;
+    //If staff is not pulling the tickets, throw error
     if (!client) throw new StatusError(403, 'Unauthorized');
     const ticket = await TicketModel.findById(req.query.id);
+    //If GET used, send ticket
     if (method == 'GET') {
         return res.json({
             ticket
         })
+        //If Patch is used
     } else if (method == 'PATCH') {
         await ticket.audit({
             user: client.id,
         })
+        //Assign ticket assignee to the staff changing the status
         UpdateDocument(ticket, req.body);
         if ('status' in req.body) {
             ticket.assignee = client.id;
         }
+        //Show its updated
         await ticket.commitWith(req, {
             action: 'Updated Ticket',
         });
+        //Save changed ticket and send
         await ticket.save();
         return res.json({
             ticket
