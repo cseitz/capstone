61f3a555fcd41176ae4a7723c373d36b3ee3d203
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-14 15:18:04 -0400

    removed sample data

--- src/app/pages/api/details/index.ts
+++ src/app/pages/api/details/index.ts
@@ -5,14 +5,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export const FillableTextData = {
     landing: {
-        title: "NEVER FORGET",
-        subtitle: "Sep 11th 2001",
+        title: "",
+        subtitle: "",
         backgroundImage: "",
         logo: "/assets/logo.png",
     },
     about: {
-        info: "No Maidens?",
-        content: "This is more information to be put into another box",
+        info: "",
+        content: "",
         image: "",
         backgroundImage: ""
     }
