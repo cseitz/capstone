6e937a24d1d020acb3a4aefd43d4b017d1ecad49
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-15 11:40:09 -0400

    fixed role update token

--- src/staff/pages/api/auth/index.ts
+++ src/staff/pages/api/auth/index.ts
@@ -1,13 +1,30 @@
 import { Route, StatusError } from "lib/route";
-import { verifyToken } from "lib/auth";
+import { createToken, isAuthenticated, verifyToken } from "lib/auth";
+import { UserModel } from "lib/mongo/schema/user";
+import { serialize } from "cookie";
 
 export interface AuthenticationStatusResponse {
 
 }
 
+const isLoggedIn = isAuthenticated({});
+
 export default Route(async (req, res) => {
-    const token = await verifyToken(req);
-    if (token)
+    let token = await verifyToken(req);
+    if (token) {
+        const client = isLoggedIn(req);
+        if (client) {
+            const user = await UserModel.findById(client.id);
+            const { role } = user;
+            if (role != client.role) {
+                const refreshedToken = createToken(user, token);
+                res.setHeader('Set-Cookie', serialize('auth', refreshedToken, { path: '/' })).json({
+                    token,
+                });
+                token = await verifyToken(refreshedToken);
+            }
+        }
         return res.json({ token });
+    }
     throw new StatusError(401, 'Unauthorized');
 });
