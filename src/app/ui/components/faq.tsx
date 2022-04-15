3f8effd9816e5908c31bc5c51c5f2f4d1e8282c2
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-15 13:33:38 -0400

    faq details api

--- src/app/ui/components/faq.tsx
+++ src/app/ui/components/faq.tsx
@@ -1,48 +1,34 @@
 import { useState } from 'react'
 import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-
-//Add New FAQ Entries Here
-const faqs = [
-  {
-    question: "How long did it take to make this accordion?",
-    answer: "Too long."
-  },
-  {
-    question: "Who made it?",
-    answer: "I did."
-  },
-  {
-    question: "Who are you?",
-    answer: "Jake, from StateFarm."
-  },
-  {
-    question: "What are you wearing, Jake from StateFarm",
-    answer: "Nothing. B)"
-  }
-]
+import { useQuery } from 'react-query';
+import type { FAQDetails } from 'staff/pages/api/details';
 
 
 export function FAQ() {
-  const [expanded, setExpanded] = useState<number>(-1);
+    const [expanded, setExpanded] = useState<number>(-1);
+    //pulling data from api about the details
+    const { isLoading, error, data = { questions: [] } } = useQuery<FAQDetails>(['details', 'faq'], () => {
+        return fetch('/api/details/faq')
+            .then(response => response.json())
+    })
+    const { questions = [] } = data;
 
-  const handleChange =
-    (panel: number) => (event: React.SyntheticEvent, newExpanded: boolean) => {
-      setExpanded(newExpanded ? panel : -1);
+    const handleChange = (panel: number) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+        setExpanded(newExpanded ? panel : -1);
     };
 
-  const info = faqs.map((x, i) =>
-    <Accordion expanded={expanded === i} onChange={handleChange(i)} key={x.question}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel-content" id="panel-header">
-        {x.question}
-      </AccordionSummary>
-      <AccordionDetails>
-        {x.answer}
-      </AccordionDetails>
-    </Accordion>
-  );
+    const info = questions.map((x, i) =>
+        <Accordion expanded={expanded === i} onChange={handleChange(i)} key={x.id}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel-content" id="panel-header">
+                {x.question}
+            </AccordionSummary>
+            <AccordionDetails>
+                {x.answer}
+            </AccordionDetails>
+        </Accordion>
+    );
 
-  return (<div>{info}</div>);
+    return (<div>{info}</div>);
 
 }
