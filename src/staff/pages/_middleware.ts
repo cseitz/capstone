e566b03907896f0f07314781fd20ded88f651908
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-16 14:48:26 -0400

    fixed redirect logic

--- src/staff/pages/_middleware.ts
+++ src/staff/pages/_middleware.ts
@@ -5,20 +5,19 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export default function handler(req: NextRequest, ev: NextFetchEvent) {
     const { nextUrl: { pathname }, cookies: { auth } } = req;
-    console.log('visit', pathname, isLoggedIn(auth));
-    // if (pathname.startsWith('/assets'))
-    //     return NextResponse.next();
-    // if (pathname.includes('/api'))
-    //     return NextResponse.next();
-    // if (pathname == '/login') {
-    //     if (isStaff(auth))
-    //         return NextResponse.redirect('/', 307);
-    //     else if (isLoggedIn(auth))
-    //         return NextResponse.next();
-    // };
-    // if (isStaff(auth))
-    //     return NextResponse.next();
-    // if (pathname != '/login')
-    //     return NextResponse.redirect('/login', 307);
-    return NextResponse.next();
+    // console.log('visit', pathname, isLoggedIn(auth));
+    if (pathname.startsWith('assets') || pathname.startsWith('/api') || pathname.startsWith('/logout'))
+        return NextResponse.next();
+    if (pathname == '/login')
+        if (isStaff(auth))
+            return NextResponse.redirect('/', 307);
+        else if (isLoggedIn(auth))
+            return NextResponse.rewrite('/denied');
+        else
+            return NextResponse.next();
+    if (isStaff(auth))
+        return NextResponse.next();
+    if (isLoggedIn(auth))
+        return NextResponse.rewrite('/denied');
+    return NextResponse.redirect('/login', 307);
 }
