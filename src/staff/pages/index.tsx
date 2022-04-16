3a15d52269924333a583ecaba455f8325adb1d34
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-04-16 14:59:01 -0400

    adjusted user list padding

--- src/staff/pages/index.tsx
+++ src/staff/pages/index.tsx
@@ -4,7 +4,7 @@ import { UserList } from "ui/components/user";
 
 
 export default function Homepage() {
-    return <Box sx={{ p: 2 }}>
+    return <Box sx={{ p: 2, maxWidth: '1000px', mx: 'auto' }}>
         <Typography variant="h4">Users</Typography>
         <UserList showCount />
     </Box>
